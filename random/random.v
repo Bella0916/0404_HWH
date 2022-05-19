16d914ba7504826288502cb33c13eb8146730cea
Bella0916 <102302216+Bella0916@users.noreply.github.com>
2022-05-20 02:04:52 +0800

    Update random.v

--- random/random.v
+++ random/random.v
@@ -3,10 +3,10 @@ module random(clk,clr,X);
   input clr;
   output X;
   reg[2:0] Q;        //3個寄存器
-  assign X=Q[0];     //輸出連結
+  assign X=Q[0];   
 always@(negedge clk or posedge clr)
   if(clr)begin
-    Q<=4'b1111;      //不0000
+    Q<=4'b1111;      //n 0000
   end
   else begin
     Q[1:0]<=Q[2:1];
